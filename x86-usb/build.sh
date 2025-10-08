2dceaeb1c3926f8cdbd95199e85e0224c7f1036d
forker-25 <gerni780@gmail.com>
2025-10-08 11:59:28 +0300

    Update build.sh

--- x86-usb/build.sh
+++ x86-usb/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Building NaOS..."
+# build.sh maded by cirnik for ArgOS-usb-x86
+
+echo "Building ArgOS-usb-x86..."
 mkdir -p releases
+
 echo "Compiling bootloader..."
 nasm -f bin src/boot.asm -o releases/boot.bin
 if [ $? -ne 0 ]; then
@@ -9,11 +12,21 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+echo "Compiling programs..."
+nasm -f bin src/devs.asm -o releases/devs.bin
+if [ $? -ne 0 ]; then
+    echo "Error building devs.bin"
+    exit 1
+fi
+
+OUTPUT="releases/naos.usb" 
+FORMAT="usb" # usb, iso or img only
+
 echo "Creating disk image..."
-python3 src/create.py releases/boot.bin releases/naos.img
+python3 src/create.py releases/boot.bin "$OUTPUT" releases/devs.bin --format "$FORMAT"
 
 if [ $? -eq 0 ]; then
-    echo "Build complete! Created releases/naos.img"
+    echo "Build complete! Created $OUTPUT"
 else
     echo "Error creating disk image"
     exit 1
